e877ab20f4e29016141d9400b4f62af9080a0868
akshatsinghkaushik <akshatsingh99@gmail.com>
2019-10-05 15:00:19 +1000

    Init

---
+++ js/config.js
@@ -0,0 +1,7 @@
+window._config = {
+    cognito: {
+        userPoolId: 'ap-southeast-2_BWVlGyzlK', // e.g. us-east-2_uXboG5pAb
+        region: 'ap-southeast-2', // e.g. us-east-2
+		clientId: '1j8fmnhhc5spbpsobdl9fjrmgs' //is this used anywhere?
+    },
+};
